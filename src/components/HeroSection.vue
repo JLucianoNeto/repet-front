907086208064980e778b8107a0acc34fc51de005
Ubiracyauri Neto <bibineto2018@gmail.com>
2025-02-23 18:22:30 -0300

    feat: página HomePage criada

---
+++ src/components/HeroSection.vue
@@ -0,0 +1,92 @@
+<template>
+  <section class="section-container">
+    <div class="hero">
+      <div class="hero-text">
+        <h2>Um novo<br>conceito de <br><div class="highlight"><h2>Reciclagem</h2> <img src="@/assets/images/svg-reciclagem.svg" alt=""></div></h2>
+        <p>Repense, Recicle e Reuse.</p>
+        <a href="#impressoes" class="catalog-button">Catálogo</a>
+      </div>
+      <div class="hero-image">
+        <img src="@/assets/images/Bgirl.png" alt="Hero Image">
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.section-container{
+  margin-top: 7rem;
+  padding: 5rem;
+}
+.hero {
+  display: flex;
+  align-items: center;
+  background-color: #F4F6F5;
+  padding: 2.5rem;
+  border-radius: 1.563rem;
+  width: max-content;
+  width: 80%;
+}
+.hero-text {
+  width: 50%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+}
+
+.hero-text p {
+  font-size: 3rem;
+}
+
+.hero h2, .highlight h2 {
+  font-size: 10rem;
+  font-weight: 900;
+}
+
+.hero h2 {
+  line-height: 11.188rem;
+}
+
+.highlight {
+  position: relative;
+  z-index: 2;
+}
+
+.highlight img {
+  position: absolute;
+  z-index: -1;
+  bottom: 0;
+  width: 64rem;
+}
+
+.catalog-button {
+  margin-top: 0.625rem;
+  background-color: black;
+  color: white;
+  padding: 0.625rem 1.25rem;
+  border: none;
+  border-radius: 0.513rem;
+  cursor: pointer;
+  width: max-content;
+  font-size: 3rem;
+}
+
+.catalog-button:hover {
+  opacity: 0.6;
+}
+
+.section-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.hero-image{
+  width: 50%;
+}
+
+.hero-image img{
+  width: 100%;
+}
+</style>
---
+++ src/components/ModelCard.vue
@@ -0,0 +1,51 @@
+<script setup>
+  defineProps({
+    image: String,
+    title: String,
+    description: String,
+  });
+</script>
+
+<template>
+  <div class="card">
+    <img :src="image" :alt="title" class="card-image" />
+    <h3 class="card-title">{{ title }}</h3>
+    <p class="card-description">{{ description }}</p>
+    <a href="#" class="card-link">→</a>
+  </div>
+</template>
+
+<style>
+.card {
+  background: white;
+  padding: 1.5rem;
+  border-radius: 0.625rem;
+  box-shadow: 0 0.25rem 0.5rem rgba(0, 0, 0, 0.1);
+  text-align: center;
+  width: 33%;
+}
+.card-image {
+  width: 100%;
+  border-radius: 0.625rem;
+}
+.card-title {
+  font-size: 1.25rem;
+  margin: 0.75rem 0;
+}
+.card-description {
+  color: #666;
+  font-size: 1rem;
+}
+
+.card h3, .card p{
+  font-size: 2.25rem;
+}
+
+.card-link {
+  display: block;
+  margin-top: 0.5rem;
+  font-size: 3rem;
+  text-decoration: none;
+  color: black;
+}
+</style>
