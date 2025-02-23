bc4632e71067ffb6602a095ea2d06a3a87b22eff
JLucianoNeto <101523394+JLucianoNeto@users.noreply.github.com>
2025-02-23 17:54:53 -0300

    feat: função de registro da api implementada

--- src/api/index.js
+++ src/api/index.js
@@ -1,27 +1,66 @@
-import axios from "axios";
+import axios from 'axios'
 
-export const BASE_URL = 'http://localhost:1337';
+export const BASE_URL = 'http://localhost:1337'
 
 export const api = axios.create({
   baseURL: `${BASE_URL}/api`,
   // timeout: 1000
 })
 
-
-
 export async function login(email, password) {
-  const response = await api.post(
-    '/login',
-    {
-      email,
-      password,
-    }
-  );
+  const response = await api.post('/login', {
+    email,
+    password,
+  })
 
-  if (response.status !== 202){
-    return;
+  if (response.status !== 202) {
+    return
   }
 
-  return response.data;
+  return response.data
+}
 
+export async function register(username, name, email, matricula, phone, password) {
+  const response = await api.post('/register', {
+    username,
+    name,
+    email,
+    matricula,
+    phone,
+    password,
+  })
+
+  if (response.status !== 202) {
+    return
+  }
+
+  return response.data
+}
+export async function createModel(username, name, email, matricula, phone, password) {
+  const response = await api.post('/register', {
+    username,
+    name,
+    email,
+    matricula,
+    phone,
+    password,
+  })
+
+  if (response.status !== 202) {
+    return
+  }
+
+  return response.data
+}
+
+export async function deleteModel(id) {
+  const response = await api.post('/delete', {
+    id,
+  })
+
+  if (response.status !== 202) {
+    return
+  }
+
+  return response.status
 }
