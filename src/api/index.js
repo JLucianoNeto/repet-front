a0a0b9c612d589d930affa2ba21f81cd1fbd6450
JLucianoNeto <101523394+JLucianoNeto@users.noreply.github.com>
2025-02-23 00:16:42 -0300

    feat: criação da função login para a api

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,27 @@
 import axios from "axios";
 
-export const BASE_URL = 'http://localhost:1337'
+export const BASE_URL = 'http://localhost:1337';
 
 export const api = axios.create({
   baseURL: `${BASE_URL}/api`,
   // timeout: 1000
 })
+
+
+
+export async function login(email, password) {
+  const response = await api.post(
+    '/login',
+    {
+      email,
+      password,
+    }
+  );
+
+  if (response.status !== 202){
+    return;
+  }
+
+  return response.data;
+
+}
