ae987d78fb108fe6a4e384090f7bb6ca2b75b608
Jphn <joaopedroholandaneves@gmail.com>
2025-02-25 13:36:54 -0300

    fix(api): corrigir arquivo da api

--- src/api/index.js
+++ src/api/index.js
@@ -5,7 +5,6 @@ export const BASE_URL = 'http://localhost:5500'
 export const api = axios.create({
   baseURL: `${BASE_URL}`,
   validateStatus: (status) => status >= 200 && status <= 500,
-  // timeout: 1000
 })
 
 export async function login(email, password) {
@@ -28,7 +27,7 @@ export async function register(
   password,
   birthdate,
 ) {
-  const response = await api.post('/users', {
+  const response = await api.post('/register', {
     username,
     display_name: displayName,
     email,
@@ -46,7 +45,7 @@ export async function register(
 export async function storeModel(token, name, description, credits, privateValue) {
   const response = await api.post(
     '/models',
-    { name, description, credits, privateValue },
+    { name, description, credits, private: privateValue },
     {
       headers: {
         Authorization: `Bearer ${token}`,
@@ -54,14 +53,20 @@ export async function storeModel(token, name, description, credits, privateValue
     },
   )
 
-  return {
-    success: response.status === 201,
-    data: response.data,
-  }
+  return response.status === 201 ? response.data : false
+}
+
+export async function getAllModels(token) {
+  const response = await api.get('/models', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
+
+  return response.status === 200 ? response.data : false
 }
 
 export async function getModelById(token, id) {
-  console.log(`Bearer ${token}`)
   const response = await api.get(`/models/${id}`, {
     headers: {
       Authorization: `Bearer ${token}`,
@@ -74,7 +79,7 @@ export async function getModelById(token, id) {
 export async function updateModel(token, id, name, description, credits, privateValue) {
   const response = await api.put(
     `/models/${id}`,
-    { name, description, credits, privateValue },
+    { name, description, credits, private: privateValue },
     {
       headers: {
         Authorization: `Bearer ${token}`,
